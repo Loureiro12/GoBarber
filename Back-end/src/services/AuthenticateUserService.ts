88f3c2eaa6d101a4cb8b5f859de25d70366f36e3
Loureiro12 <contato.loureiro1@gmail.com>
2021-01-15 23:23:05 -0300

    Gerando token JWT

--- Back-end/src/services/AuthenticateUserService.ts
+++ Back-end/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from "typeorm";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 import User from "../models/User";
 
@@ -8,8 +9,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{ user: User }> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -24,8 +30,14 @@ class AuthenticateUserService {
       throw new Error("Incorrect email/password combination.");
     }
 
+    const token = sign({}, "5be9a654338f30a7add76493a15ad614", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
     return {
       user,
+      token,
     };
   }
 }
