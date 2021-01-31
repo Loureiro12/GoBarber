7e5c9b85a44fae34cb81b1c3d1ff43a203467d3f
André Loureiro <contato.loureiro1@gmail.com>
2021-01-31 17:30:08 -0300

    Error styled-componens

--- appgoBarber/src/App.tsx
+++ appgoBarber/src/App.tsx
@@ -3,7 +3,7 @@ import {StatusBar, View} from 'react-native';
 
 const App: React.FC = () => (
   <>
-    <StatusBar />
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
     <View style={{flex: 1, backgroundColor: '#312e38'}} />
   </>
 );
